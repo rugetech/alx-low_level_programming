3eefed653e0c23e5277f4ccb02533126aa9112b2
rugetech <alistidesrugemalila127@gmail.com>
2023-03-17 18:45:18 +0300

    C program

--- 0x01-variables_if_else_while/0-positive_or_negative.c
+++ 0x01-variables_if_else_while/0-positive_or_negative.c
@@ -16,12 +16,19 @@ srand(time(0));
 n = rand() - RAND_MAX / 2;
 /*Generates random numbers */
 if (n > 0)
+{
 	printf("The number is positive. \n");
+}
 
 else if (n < 0)
+{
 	printf("The number is negative. \n");
+}
 
 else
+{
 	printf("The number is zero. \n");
+}
+
 return (0);
 }
