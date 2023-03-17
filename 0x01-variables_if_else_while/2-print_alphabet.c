5db04d465128b92f4735b5ba7411b193207ab1d3
rugetech <alistidesrugemalila127@gmail.com>
2023-03-17 23:03:34 +0300

    C program

--- 0x01-variables_if_else_while/2-print_alphabet.c
+++ 0x01-variables_if_else_while/2-print_alphabet.c
@@ -5,14 +5,13 @@
  * Return: Always 0 (Success)
  */
 
-int main() {
-	  char c;
-
-	    for (c = 'a'; c <= 'z'; c++) {
-		        putchar(c);
-			  }
-
-	      putchar('\n');
-
-	        return 0;
+int main(void)
+{
+char c;
+for (c = 'a'; c <= 'z'; c++)
+{
+putchar(c);
+}
+putchar('\n');
+return (0);
 }
