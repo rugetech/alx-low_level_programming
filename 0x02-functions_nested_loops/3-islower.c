510ced045c872dbc364ad244cf328975f776ec6a
rugetech <alistidesrugemalila127@gmail.com>
2023-03-21 16:09:41 +0300

    C functions

---
+++ 0x02-functions_nested_loops/3-islower.c
@@ -0,0 +1,19 @@
+#include "main.h"
+
+/**
+ * _islower -function
+ * @c: range of characters
+ * Return: 1 or 0
+*/
+
+int _islower(int c)
+{
+if (c >= 'a' && c <= 'z')
+{
+return (1);
+}
+else
+{
+return (0);
+}
+}
---
+++ 0x02-functions_nested_loops/4-isalpha.c
@@ -0,0 +1,18 @@
+#include "main.h"
+
+/**
+* _isalpha - function
+* @c: checks if character is among letters.
+* Return: 1 or 0
+*/
+int _isalpha(int c)
+{
+if ((c >= 'a' && c <= 'z') || (c >= 'A' && c <= 'Z'))
+{
+return (1);
+}
+else
+{
+return (0);
+}
+}
